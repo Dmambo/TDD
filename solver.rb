615135eca766d4fe302599a88ab16e62c1814163
tsohleDev <mokhemisitsohle@gmail.com>
2023-03-23 16:27:11 +0200

    Write specs

--- solver.rb
+++ solver.rb
@@ -1,21 +1,23 @@
 class Solver
-    def factorial(n)
-      raise ArgumentError.new("n must be non-negative") if n < 0
-      return 1 if n == 0
-      (1..n).inject(:*)
-    end
-  
-    def reverse(word)
-      word.reverse
-    end
-  
-    def fizzbuzz(n)
-      case 0
-      when n % 15 then 'fizzbuzz'
-      when n % 3 then 'fizz'
-      when n % 5 then 'buzz'
-      else n.to_s
-      end
+  def self.factorial(number)
+    raise ArgumentError, 'n must be non-negative' if number.negative?
+    return 1 if number.zero?
+
+    (1..number).inject(:*)
+  end
+
+  def self.reverse(word)
+    raise ArgumentError, 'Invalid input' if word.class != String
+
+    word.reverse
+  end
+
+  def self.fizzbuzz(number)
+    case number
+    when number % 15 then 'fizzbuzz'
+    when number % 3 then 'fizz'
+    when number % 5 then 'buzz'
+    else number.to_s
     end
   end
-  
+end
